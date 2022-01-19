f0668b8411fca7258d2057c4f69b31f29fde8ef9
SuyeonSun <suninisy@gmail.com>
2022-01-19 10:40:51 +0900

    today's commit

--- b4344/app.js
+++ b4344/app.js
@@ -1,8 +1,23 @@
 var fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-// const input = fs.readFileSync('/dev/stdin).toString().split('\n');
-var cnt = 0;
-var total = 0;
-var avg = 0;
 
+for (var i=1; i<=input[0]; i++) {
+    var numbers = input[i].split(' ').map(Number);
+    var cnt = numbers[0];
+    var sum = numbers.reduce((a, b) => a+b, -cnt); 
+    // 초기값은 - cnt : let sum = numbers.reduce((a, b) => a+b, -numbers[0]);
+    // let sum = numbers.reduce((a, b) => a+b) - numbers[0];
+    var avg = sum / cnt;
+    
+    var aboveAvg = 0;
+    for (var j=0; j<=cnt; j++) {
+        if (numbers[j] > avg) {
+            aboveAvg ++;
+        }
+    }
+    var result = ((aboveAvg/cnt)*100).toFixed(3)
+    console.log(result + '%')
+}
+
+// https://gurtn.tistory.com/45
