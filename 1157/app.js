e35a01966dd561b059580b476926abec230a0314
SuyeonSun <suninisy@gmail.com>
2022-01-24 11:17:01 +0900

    .

---
+++ 1157/app.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+// const input = fs.readFileSync('/dev/stdin).toString().split('\n');
+
+console.log(input)
